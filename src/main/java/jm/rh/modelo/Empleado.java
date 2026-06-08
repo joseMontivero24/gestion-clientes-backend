11751ee2f000632f387edf5e48431932e345af89
Jose Montivero <chipimontivero2019@gmail.com>
2026-06-08 09:04:29 -0300

    Proyect Inizializer con Spring Boot

---
+++ src/main/java/jm/rh/modelo/Empleado.java
@@ -0,0 +1,17 @@
+package jm.rh.modelo;
+
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Empleado {
+
+}
