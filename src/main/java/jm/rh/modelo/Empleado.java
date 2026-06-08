a3a9ca4e69bb62039823ba53f93647836764e123
Jose Montivero <chipimontivero2019@gmail.com>
2026-06-08 09:58:11 -0300

    lista la conexion a MySQL y lista la clase de entity

--- src/main/java/jm/rh/modelo/Empleado.java
+++ src/main/java/jm/rh/modelo/Empleado.java
@@ -2,6 +2,9 @@ package jm.rh.modelo;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,5 +16,10 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 public class Empleado {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer idEmpleado;
+    String nombre;
+    String departamento;
+    Double sueldo;
 }
