350963fa73b46629a2e2d4d4f07727a8595eb4d2
Jose Montivero <chipimontivero2019@gmail.com>
2026-06-15 08:51:52 -0300

    metodo obtenerEmpleadoPorId para buscar empleado por id, agregado en controller

--- src/main/java/jm/rh/controller/EmpleadoControlador.java
+++ src/main/java/jm/rh/controller/EmpleadoControlador.java
@@ -1,12 +1,15 @@
 package jm.rh.controller;
 
 import java.util.List;
+
+import jm.rh.exepcion.RecursoNoEncontradoExepcion;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import jm.rh.modelo.Empleado;
 import jm.rh.service.IEmpleadoServicio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -34,4 +37,14 @@ public class EmpleadoControlador {
         logger.info("Empleado a agregar: " + empleado);
         return empleadoServicio.guardarEmpleado(empleado);
     }
+
+    @GetMapping("/empleados/{id}")
+    public ResponseEntity<Empleado> obtenerEmpleadoPorId(@PathVariable Integer id) {
+        Empleado empleado = empleadoServicio.buscarEmpleadoPorId(id);
+        if (empleado == null) {
+            throw new RecursoNoEncontradoExepcion("Empleado no encontrado po ID " + id);
+        }
+            return ResponseEntity.ok(empleado);
+    }
 }
+// ME QUEDE EN EL VIDEO 405
---
+++ src/main/java/jm/rh/exepcion/RecursoNoEncontradoExepcion.java
@@ -0,0 +1,11 @@
+package jm.rh.exepcion;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class RecursoNoEncontradoExepcion extends RuntimeException{
+    public RecursoNoEncontradoExepcion(String mensaje){
+        super(mensaje);
+    }
+}
